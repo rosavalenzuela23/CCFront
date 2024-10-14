e486c0717907613a1cd3541b3ebdf67b00d3712e
rosa <natsukiarr@gmail.com>
2024-10-14 16:04:31 -0700

    ADD expediente-view-component
    agregue la estructura base de la pantalla

--- CCFront/src/app/app.routes.ts
+++ CCFront/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import {GestionViewComponent} from "./components/gestion-view/gestion-view.component";
 import {MainViewComponent} from "./components/main-view/main-view.component";
+import { ExpedienteViewComponent } from './components/expediente-view/expediente-view.component';
 
 export const routes: Routes = [
     {path: "", component: MainViewComponent},
-    {path: "gestion", component: GestionViewComponent}
-    
+    {path: "gestion", component: GestionViewComponent},
+    {path: "expediente", component: ExpedienteViewComponent},
 ];
