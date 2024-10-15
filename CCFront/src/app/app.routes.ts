fb7a30097b9377517c4e3402ac57e3397ca10f2a
rosa <natsukiarr@gmail.com>
2024-10-14 22:32:31 -0700

    FIX routes error
    Faltaban clases en el archivo de rutas

--- CCFront/src/app/app.routes.ts
+++ CCFront/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {GestionViewComponent} from "./components/gestion-view/gestion-view.component";
 import {MainViewComponent} from "./components/main-view/main-view.component";
+import { SesionesViewComponent } from './components/sesiones-view/sesiones-view.component';
+import { SesionViewComponent } from './components/sesion-view/sesion-view.component';
+import { ExpedienteViewComponent } from './components/expediente-view/expediente-view.component';
 
 export const routes: Routes = [
     {path: "", component: MainViewComponent},
