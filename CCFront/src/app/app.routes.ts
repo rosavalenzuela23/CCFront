98d7e8c45ccb414ed59ad02ffc75e0d0bc34755b
Marcos-Ruiz1 <Marcos.ZazuetaRuiz1@gmail.com>
2024-10-14 17:57:46 -0700

    add diseño "atender paciente" componente
    Se agrego la estructura html y el diseño css de una alerta que en teoría se muestra cuando te llega  un paciente pero sin la logica de a donde mandarte

--- CCFront/src/app/app.routes.ts
+++ CCFront/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {GestionViewComponent} from "./components/gestion-view/gestion-view.component";
 import {MainViewComponent} from "./components/main-view/main-view.component";
 import { ExpedienteViewComponent } from './components/expediente-view/expediente-view.component';
+import {SesionesViewComponent} from "./components/sesiones-view/sesiones-view.component";
 
 export const routes: Routes = [
     {path: "", component: MainViewComponent},
     {path: "gestion", component: GestionViewComponent},
     {path: "expediente", component: ExpedienteViewComponent},
+    {path: "sesiones/:id", component: SesionesViewComponent}
 ];
--- CCFront/src/app/components/gestion-view/tabla-pacientes/tabla-pacientes.component.ts
+++ CCFront/src/app/components/gestion-view/tabla-pacientes/tabla-pacientes.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'tabla-pacientes-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './tabla-pacientes.component.html',
   styleUrl: './tabla-pacientes.component.css'
 })
 export class TablaPacientesComponent {
+
+  id: number = 0;
+
 
 }
