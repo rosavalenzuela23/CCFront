43d1ad35a3a9d9411e40cbceb22c5da3aa46c459
rosa <natsukiarr@gmail.com>
2024-12-05 11:01:04 -0700

    ADD: Happy path for adding cartas consentimiento

--- CCFront/src/app/components/consentimiento-view/consentimiento-view.component.ts
+++ CCFront/src/app/components/consentimiento-view/consentimiento-view.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CartaPacienteComponent } from './carta-paciente/carta-paciente.component';
+import { PacienteService } from '../../services/PacienteService';
+import { Observable } from 'rxjs';
+import { DtoPaciente } from '../../dtos/DtoPaciente';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-consentimiento-view',
   standalone: true,
-  imports: [CartaPacienteComponent],
+  imports: [CartaPacienteComponent, AsyncPipe],
   templateUrl: './consentimiento-view.component.html',
   styleUrl: './consentimiento-view.component.css'
 })
 export class ConsentimientoViewComponent {
+
+  listaPacientes$!: Observable<DtoPaciente[]>;
+
+  constructor(
+    private pacienteService: PacienteService
+  ){}
+
+  ngOnInit() {
+    this.listaPacientes$ = this.pacienteService.obtenerPacientesSinCarta();
+  }
 
 }
