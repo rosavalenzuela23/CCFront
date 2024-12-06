1856e892be1628e10419c970aad07142e61e23d6
Marx_Mas <116768281+LuisReynoso233531@users.noreply.github.com>
2024-12-05 23:38:54 -0700

    Update login-view.component.ts

--- CCFront/src/app/components/login-view/login-view.component.ts
+++ CCFront/src/app/components/login-view/login-view.component.ts
@@ -20,33 +20,75 @@ type errorType = {
   styleUrls: ['./login-view.component.css']
 })
 export class LoginViewComponent {
-
-  formUser = new FormGroup({
-
-    'username': new FormControl('', Validators.required),
-    'password': new FormControl('', [Validators.required,
-      Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
-    ])
-  });
+  
+    mensajeError!: string;
 
 
-  isPasswordLongEnough(): boolean {
-    return (this.formUser.get('password')?.value ?? '').length >= 8;
+    //Lista de errores a mostrar en la vista
+   errores: errorType[] = [];
+
+    // Formulario reactivo
+    loginForm = new FormGroup({
+        userElement: new FormControl('', [Validators.required]), // Campo para el usuario
+        passwordElement: new FormControl('', [Validators.required])  // Campo para la contraseña
+    });
+
+    constructor(private authService: AuthService) {}
+
+    iniciarSesion() {
+
+        if (this.loginForm.invalid) {
+            this.validarCamposLogIn();
+            return;
+        }
+        
+        const usuario: string = this.loginForm.get('userElement')?.value || "";
+        const password: string = this.loginForm.get('passwordElement')?.value || "";
+           
+        this.authService.logIn(usuario, password).catch((error: string)=>{
+            this.mensajeError = error;
+
+        })
+    }
+
+
+    //Agrega un error a la lista de errores para mostrar en la vista
+    private agregarError(controlName: string, validator: string){
+
+        this.errores.push({
+        idComponente:`${controlName}+${validator}+${Date.now()}_${Math.random()}`,
+        message: `${ControlName[controlName]} no cumple: ${ControlNameValidator[validator]}`,
+        title: `Error en el campo ${ControlName[controlName]}`
+        })
+        console.log(this.errores);
+    }
+
+    // Valida los controles dentro de un FormGroup y agrega errores según corresponda
+    private obtenerValidadores(controladoresFormGroup: string[], formGroup: FormGroup) {
+
+        for (const controlName of controladoresFormGroup) {
+
+        const formControl = formGroup.get(controlName);
+        const validators: string[] = Object.getOwnPropertyNames(formControl?.errors || {})
+      
+        for (const validator of validators) {
+
+            this.agregarError(controlName, validator);
+        
+        }
+
+        }
+    }
+
+    // Valida todos los controles en el formulario principal, incluyendo FormArrays, FormGroups y FormControls
+    private async validarCamposLogIn() {
+    
+        this.errores = [];
+        const controles: string[] = Object.getOwnPropertyNames(this.loginForm.controls);
+        for (const controlName of controles) {
+            this.obtenerValidadores([controlName], this.loginForm);
+        }
+
   }
 
-  hasLowerCase(): boolean {
-    return /[a-z]/.test(this.formUser.get('password')?.value || '');
-  }
-
-  hasUpperCase(): boolean {
-    return /[A-Z]/.test(this.formUser.get('password')?.value || '');
-  }
-
-  hasNumber(): boolean {
-    return /\d/.test(this.formUser.get('password')?.value || '');
-  }
-
-  hasSpecialCharacter(): boolean {
-    return /[!@#$%^&*()_+\[\]{};':"\\|,.<>\/?-]/.test(this.formUser.get('password')?.value || '');
-  }
 }
