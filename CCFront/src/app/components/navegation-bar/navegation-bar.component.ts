dd9128940bcd9171266f28f4b393232336e74800
rosa <natsukiarr@gmail.com>
2024-12-06 00:15:11 -0700

    ADD: Way to identify users roles

--- CCFront/src/app/components/navegation-bar/navegation-bar.component.ts
+++ CCFront/src/app/components/navegation-bar/navegation-bar.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { PacienteService } from '../../services/PacienteService';
 import { AuthService } from './../../services/auth.service';
+import { StompserviceService } from '../../services/stompservice.service';
+import { SessionStorageNames } from '../../services/sessionStorageNames';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navegation-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navegation-bar.component.html',
   styleUrl: './navegation-bar.component.css'
 })
@@ -17,7 +20,8 @@ export class NavegationBarComponent {
 
   constructor(
     private pacienteService: PacienteService,
-    private authService: AuthService
+    private authService: AuthService,
+    private stomp: StompserviceService
   ){}
 
   buscarExpedientes() {
@@ -26,7 +30,16 @@ export class NavegationBarComponent {
 
   }
 
+  tienePermisos(permisoRequerido: string): boolean {
+    const token = sessionStorage.getItem(SessionStorageNames.TOKEN);
+    if (token == null) {
+      return false;
+    }
+    return token === permisoRequerido;
+  }
+
   logOut(){
+    this.stomp.desconectar();
     this.authService.logOut();
   }
 
