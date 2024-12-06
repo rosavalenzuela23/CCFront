2d8b91a510579bb422708220dffc9922fba35746
rosa <natsukiarr@gmail.com>
2024-12-06 02:02:01 -0700

    ADD: FUNCTIONALITY TO REACT TO A MESSAGE

--- CCFront/src/app/components/gestion-view/ventana-atender/ventana-atender.component.ts
+++ CCFront/src/app/components/gestion-view/ventana-atender/ventana-atender.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, Router } from "@angular/router";
 import {DtoPaciente} from "../../../dtos/DtoPaciente";
@@ -14,6 +14,7 @@ import {PacienteService} from "../../../services/PacienteService";
 export class VentanaAtenderComponent {
 
   pacienteAtender: DtoPaciente | null = null;
+  @Output() nuevaCita = new EventEmitter<DtoPaciente>();
 
   constructor(private router: Router, servicePaciente:PacienteService){
     this.pacienteAtender = servicePaciente.getPaciente();
@@ -24,11 +25,13 @@ export class VentanaAtenderComponent {
     if(this.pacienteAtender == null){
       this.router.navigate(['/expediente']);
     }else{
-      this.router.navigate(['/sesion']);
+      this.nuevaCita.emit(this.pacienteAtender);
     }
 
   }
 
-
+  obtenerNombrePaciente() {
+    return this.pacienteAtender?.nombre || "Nuevo";
+  }
 
 }
